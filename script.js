eea81d7420f409ee0dbb3d3907f434e3711f94a0
Sesiverso <94802700+Sesiverso@users.noreply.github.com>
2024-08-28 08:03:25 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function submitQuiz() {
+    alert("Thank you for submitting the quiz! Your answers have been recorded.");
+    // Aqui você pode adicionar lógica para coletar respostas, verificar respostas corretas e exibir resultados.
+}
